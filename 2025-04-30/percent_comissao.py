b485d0031db9bcbed25d8c41cad6bd939bf514ab
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-04-30 17:47:49 -0300

    add

---
+++ 2025-04-30/percent_comissao.py
@@ -0,0 +1,9 @@
+# fazer um programa pra solicitar o valor de uma venda e
+# o percentual da comissao e exibit o valor da comissao
+
+valor_venda = float(input('digite o valor da venda (R$): '))
+percentual = float(input('informe a comissao (%).....: '))
+
+comissao = valor_venda * percentual / 100;
+
+print(f'O valor da comissao é R${comissao:.2f}')
