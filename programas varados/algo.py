285c1f7bfd706c365476d573a00bd06caaf040b1
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-04-29 23:42:11 -0300

    input e casts

--- programas varados/algo.py
+++ programas varados/algo.py
@@ -1,3 +1,8 @@
 # vou fazer alguma coisa aq
 
 print('testando o commit\n')
+
+numero = int(input('Digite seu numero:'))
+nome = (input('digite seu nome:'))
+
+print('seu nome e numero é: ', nome, '-', numero)
