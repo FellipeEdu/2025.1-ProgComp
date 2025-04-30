2646b33f48033eaaf804f385181ba0f05dfdeb1e
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-04-30 17:33:47 -0300

    criado

---
+++ 2025-04-30/media_ifrn.py
@@ -0,0 +1,14 @@
+# Calcular media da disciplina do IFRN
+#
+# 1) solicitar ao usuario que informe suas notas 
+# 2) calcular a media do IFRN 
+# 3) Exibir a media
+#
+# Media = ((Etapa1 * 2) + (Etapa2 * 3)) / 5
+
+etapa_1 = int(input('nota 1: '))
+etapa_2 = int(input('nota 2: '))
+
+media = int(((etapa_1 * 2) + (etapa_2 * 3)) / 5)
+
+print(f'sua media: {media}')
