cd9bcb975db9ed220bbef1252d8414fd521827d6
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-04-30 17:33:40 -0300

    n sei

--- 2025-04-29/area_circulo.py
+++ 2025-04-29/area_circulo.py
@@ -1,8 +1,8 @@
 print('Calcular aula do circulo')
 
-raio = input('Digite o raio do circulo: ')
+raio = float(input('Digite o raio do circulo: '))
 
-area = 3.14 * float(raio) ** 2
+area = 3.14 * raio ** 2
 
 print('Area do circulo: ', area)
 
@@ -10,6 +10,6 @@ print('A area do circulo de raio', raio, 'é', area)
 
 #print(f'A area do circulo de raio {raio} é {area:.4f}')
 
-comprimento = 2 * 3.14 * float(raio)
+comprimento = 2 * 3.14 * raio
 
-print(f'O comprimento da circunferencia é {comprimento}')
+print(f'O comprimento da circunferencia é {comprimento:.3f}')
