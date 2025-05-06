97898ae6d52ebae2503d4f9a7e207d6d9ae486c9
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-06 17:54:25 -0300

    delta

--- 2025-05-06/viagem.py
+++ 2025-05-06/viagem.py
@@ -1,8 +1,10 @@
 Vo = int(input('vel inicial (): '))
-tempo_decorrido = int(input('tempo (): '))
+#tempo_decorrido = int(input('tempo (): '))
 acel = int(input('aceleração (m/s2): '))
 dist = int(input('distancia entre cidades (km): '))
 
-tempo_viagem = ((acel * (tempo_decorrido ** 2)) / 2) + Vo * tempo_decorrido - dist
+#tempo_viagem = ((acel * (tempo_decorrido ** 2)) / 2) + Vo * tempo_decorrido - dist
+
+delta = int()
 
 print
