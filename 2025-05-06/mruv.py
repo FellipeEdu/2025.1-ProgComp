b0daaab5958bb42baa7e87999a7784d2811a8f9a
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-06 17:31:04 -0300

    implementação de exceção

--- 2025-05-06/mruv.py
+++ 2025-05-06/mruv.py
@@ -1,7 +1,17 @@
-Vo = int(input('vel inicial: '))
-tempo = int(input('tempo: '))
-acel = int(input('aceleração: '))
+import sys
 
+Vo = int(input('vel inicial: '))
+if Vo <= 0 :
+    sys.exit('informe uma velocidade positiva')
+
+tempo = int(input('tempo: '))
+if tempo <= 0 :
+    sys.exit('informe um intervalo de tempo positivo')
+
+acel = int(input('aceleração: '))
+if acel <= 0 :
+    sys.exit('informe uma aceleração positiva')
+    
 dist = Vo * tempo + ((acel * (tempo ** 2)) / 2)
 
 print(f'distancia percorrida: {dist}')
