cffd79aa7fcc1e048a5a36b6743e5249bf374fe3
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-07 17:05:15 -0300

    sys.exit e outras coisas

--- 2025-05-06/viagem.py
+++ 2025-05-06/viagem.py
@@ -1,10 +1,21 @@
-Vo = int(input('vel inicial (): '))
-#tempo_decorrido = int(input('tempo (): '))
-acel = int(input('aceleração (m/s2): '))
+import sys
+
 dist = int(input('distancia entre cidades (km): '))
+if dist <= 0:
+    sys.exit('informe distancia positiva')
+
+dist *= 1000
+#tempo_decorrido = int(input('tempo (): '))
+Vo = int(input('vel inicial (km/h): '))
+if Vo <= 0:
+    sys.exit('informe vel inicial positiva')
+
+acel = int(input('aceleração (m/s²): '))
+if acel <= 0:
+    sys.exit('informe aceleração positiva')
 
 #tempo_viagem = ((acel * (tempo_decorrido ** 2)) / 2) + Vo * tempo_decorrido - dist
 
-delta = int(Vo ** 2 - 4* acel * dist)
+delta = int(Vo ** 2 - 4 * acel * dist)
 
 print
