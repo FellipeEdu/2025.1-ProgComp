deab0c56ec41c208ea928032cc280099ae56dafa
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-07 17:17:52 -0300

    format

--- 2025-05-06/viagem.py
+++ 2025-05-06/viagem.py
@@ -3,9 +3,8 @@ import sys
 dist = int(input('distancia entre cidades (km): '))
 if dist <= 0:
     sys.exit('informe distancia positiva')
-
-dist *= 1000
 #tempo_decorrido = int(input('tempo (): '))
+
 Vo = int(input('vel inicial (km/h): '))
 if Vo <= 0:
     sys.exit('informe vel inicial positiva')
@@ -16,6 +15,11 @@ if acel <= 0:
 
 #tempo_viagem = ((acel * (tempo_decorrido ** 2)) / 2) + Vo * tempo_decorrido - dist
 
-delta = int(Vo ** 2 - 4 * acel * dist)
+dist *= 1000
+Vo /= 3.6
 
-print
+delta = int(Vo ** 2 - 4 * acel * dist)
+if delta < 0:
+    sys.exit('não é possivel calcular')
+
+print(f'')
