5d1c58d993db6d4014ef3e2c6bcae6162e0113d6
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-08 17:53:01 -0300

    aaaaa

---
+++ 2025-05-08/class_ang.py
@@ -0,0 +1,16 @@
+import sys
+
+angA = int(input('Digite o angulo A: '))
+angB = int(input('Digite o angulo B: '))
+angC = int(input('Digite o angulo C: '))
+
+soma = int(angA + angB + angC)
+
+if soma < 180 or soma > 180:
+    sys.exit('digite valores compativeis com um triangulo')
+elif angA == 90 or angB == 90 or angC == 90:    
+    sys.exit('Triangulo Retangulo.')
+elif angA >= 90 or angB >= 90 or angC >= 90:
+    sys.exit('Obtusângulo.')
+elif angA <= 90 or angB <= 90 or angC <= 90:
+    sys.exit('Acutângulo.')
