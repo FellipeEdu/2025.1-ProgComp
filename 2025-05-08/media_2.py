1108562a8024ab4f10771c14d45629e2b535f9e1
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-08 17:22:01 -0300

    a

--- 2025-05-08/media_2.py
+++ 2025-05-08/media_2.py
@@ -10,13 +10,13 @@ etapa2 = int(input('informe a nota da etpaa 2: '))
 if etapa2 < 0 or etapa2 > 100:
     sys.exit('ERRO: nota etapa 2 invalida. Informe nota entre 0 e 100')
 
-media = (etapa1 * 2 + etapa2 * 3) / 5
-print(f'Media do aluno: {media}')
+media = round((etapa1 * 2 + etapa2 * 3) / 5, 0) # arredondando media no calculo
+print(f'Media do aluno: {media}') # verificando a media sem arredondar na formatação
 print(f'Media do aluno: {media:.0f}')
 
 if media >= 60:
     print('Aluno Aprovado.')
 elif media >= 20:
-    print('Aluno em Prova Final')
+    print('Aluno em Prova Final.')
 else:
-    print('Aluno Reprovado') 
+    print('Aluno Reprovado.') 
