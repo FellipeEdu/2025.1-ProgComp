5a4e1b04fa7cec645f1521b6ca32858c800da855
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-08 17:00:21 -0300

    criando codigo

---
+++ 2025-05-08/media_2.py
@@ -0,0 +1,21 @@
+import sys
+
+etapa1 = int(input('informe a nota da etpaa 1: '))
+
+if etapa1 < 0 or etapa1 > 100:
+    sys.exit('ERRO: nota etapa 1 invalida. Informe nota entre 0 e 100')    
+
+etapa2 = int(input('informe a nota da etpaa 2: '))
+
+if etapa2 < 0 or etapa2 > 100:
+    sys.exit('ERRO: nota etapa 2 invalida. Informe nota entre 0 e 100')
+
+media = (etapa1 * 2 + etapa2 * 3) / 5
+print(f'Media do aluno: {media:.0f}')
+
+if media >= 60:
+    print('Aluno Aprovado.')
+elif media >= 20:
+    print('Aluno em Prova Final')
+else:
+    print('Aluno Reprovado') 
