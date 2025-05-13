dae96a07e7aec65c4a00c3f64bc4cbd0db3bb999
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-13 16:58:37 -0300

    quadrantes criado

---
+++ exercicios/quadrantes.py
@@ -0,0 +1,15 @@
+import sys
+
+pontoX = int(input('digite a coordenada X:'))
+pontoY = int(input('digite a coordenada Y:'))
+
+if pontoX > 0 and pontoY > 0:
+    print(f'O ponto ({pontoX},{pontoY}) pertence ao 1° quadrante.')
+elif pontoX < 0 and pontoY > 0:
+    print(f'O ponto ({pontoX},{pontoY}) pertence ao 2° quadrante.')
+elif pontoX < 0 and pontoY < 0:
+    print(f'O ponto ({pontoX},{pontoY}) pertence ao 3° quadrante.')
+elif pontoX > 0 and pontoY < 0:
+    print(f'O ponto ({pontoX},{pontoY}) pertence ao 4° quadrante.')
+else:
+    print(f'O ponto ({pontoX},{pontoY}) é centro do plano')
