21b9f64e6fa5748edde18fc9689ed6c31a87abd6
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-13 17:10:27 -0300

    usando ValueError

--- 2025-05-13/ex_excecao.py
+++ 2025-05-13/ex_excecao.py
@@ -11,6 +11,8 @@ try:
     intDividendo = int(input('Digite o dividendo:'))
     intDivisor = int(input('Digite o dividendo:'))
     floatResultado = intDividendo / intDivisor
+except ValueError:
+    print('ERRO: informe um valor que possa ser convertido em inteiro')
 except Exception as tipoExcecao:
     print(f'valor invalido')
     print(f'ERRO: {tipoExcecao}')
