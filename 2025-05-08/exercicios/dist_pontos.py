9cfe9cdb0f20150cf8368013732b3cd9c5296b99
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-13 17:44:44 -0300

    trocando o cast pra float

--- 2025-05-08/exercicios/dist_pontos.py
+++ 2025-05-08/exercicios/dist_pontos.py
@@ -1,10 +1,11 @@
 import math
 
-pontoXa = int(input('digite a coordenada X do ponto A: '))
-pontoYa = int(input('digite a coordenada Y do ponto A: '))
-pontoXb = int(input('digite a coordenada X do ponto B: '))
-pontoYb = int(input('digite a coordenada Y do ponto B: '))
+pontoXa = float(input('digite a coordenada X do ponto A: '))
+pontoYa = float(input('digite a coordenada Y do ponto A: '))
+
+pontoXb = float(input('digite a coordenada X do ponto B: '))
+pontoYb = float(input('digite a coordenada Y do ponto B: '))
 
 dist = math.sqrt(math.pow(pontoXa - pontoXb, 2) + math.pow(pontoYa - pontoYb, 2))
 
-print(f'Distancia entre o pontos A({pontoXa},{pontoYa}) e B({pontoXb},{pontoYb}) é {dist}')
+print(f'Distancia entre o pontos A({pontoXa},{pontoYa}) e B({pontoXb},{pontoYb}) é {dist:.2f}')
