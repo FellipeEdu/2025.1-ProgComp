e03c9ca14f5cbc6717437bed89b5a7456e1241e4
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-13 17:09:10 -0300

    usando Exception e sys.exc_info

--- 2025-05-13/ex_excecao.py
+++ 2025-05-13/ex_excecao.py
@@ -1,6 +1,19 @@
-try:
+'''try:
     intNum = int(input('Digite algo:'))
 except:
     print('valor invalido')
 else:
     print(intNum)
+'''
+import sys
+
+try:
+    intDividendo = int(input('Digite o dividendo:'))
+    intDivisor = int(input('Digite o dividendo:'))
+    floatResultado = intDividendo / intDivisor
+except Exception as tipoExcecao:
+    print(f'valor invalido')
+    print(f'ERRO: {tipoExcecao}')
+    print(f'ERRO: {sys.exc_info()}')
+else:
+    print(floatResultado)
