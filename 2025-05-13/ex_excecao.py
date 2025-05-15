f395b48fe25ff67319a68eeeeeac194ea15e385c
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-15 10:43:23 -0300

    explicando melhor o codigo

--- 2025-05-13/ex_excecao.py
+++ 2025-05-13/ex_excecao.py
@@ -11,11 +11,16 @@ try:
     intDividendo = int(input('Digite o dividendo:'))
     intDivisor = int(input('Digite o dividendo:'))
     floatResultado = intDividendo / intDivisor
+
 except ValueError:  # exceção expecifica, nesse caso, de valor
     print('ERRO: informe um valor que possa ser convertido em inteiro')
+
 except ZeroDivisionError: # exceção pra divisao por zero
     print('ERRO: não existe divisão por zero')
-except: # as tipoExcecao:  # passando a informação da exceção pra uma variavel
-    print(f'ERRO: {sys.exc_info()}')    # exibe uma informação mais tecnica do erro
+
+except Exception as tipoExcecao:        # passando a informação da exceção pra uma variavel
+    print(f'ERRO: {sys.exc_info()}')    # exibe uma informação mais detalhada do erro -> "<class 'ValueError'>, ValueError("erro"), <traceback>"
+#    print(f'ERRO: {tipoExcecao}')      # exibe apenas a informação do que foi o erro -> "erro"
+
 else:
     print(floatResultado)
