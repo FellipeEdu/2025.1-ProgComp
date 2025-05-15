c7baa098fa87d1c2f2f28a15bb198bcf6302a4a4
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-15 09:31:55 -0300

    comentarios

--- 2025-05-06/viagem.py
+++ 2025-05-06/viagem.py
@@ -20,11 +20,11 @@ if delta < 0:
     sys.exit('não é possivel calcular o tempo')'''
 
 # t = (-Vo + delta ** 0.5)/(2 * acel) # ** 0.5 = raiz quadrada
-t = dist / Vo # 2,7778
+t = dist / Vo # 10000s -> 2,7778h
 
-hora = t // 3600    # estudar isso a partir daq. Essa linha transforma a hora decimal em hora inteira (2,7778h -> 2h)
-t = t % 3600        # Aqui vai pegar 
-minuto = t // 60    # 
-segundo = t % 60
+hora = t // 3600    # estudar isso a partir daq. Essa linha transforma a hora decimal em segundos em hora inteira (2,7778h (10000s) -> 2h)
+t = t % 3600        # Aqui vai pegar o resto da divisão inteira da hora pra calcular os minutos (0,7778h)
+minuto = t // 60    # Aqui vai fazer o calculo dos minutos (não deveria multiplicar??)
+segundo = t % 60    # Resto da divisão de minutos por 60 para calcular os segundos
 
 print(f'tempo restante: {hora:.0f}h:{minuto:.0f}m:{segundo:.0f}s')
