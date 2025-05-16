9dfb8d67e48fe75e825c1ffdda7b8707a35deb09
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-15 22:42:00 -0300

    add input

--- programas varados/while_loucura.py
+++ programas varados/while_loucura.py
@@ -1,4 +1,6 @@
-i = 1
-while i <= 10000:
-    print(f'atumalaca: {i}')
+i = 0
+atumalacas = int(input('quantos atumalacas vc quer: '))
+
+while i <= 10000000:
+    print(f'atumalaca {i}')
     i += 1
