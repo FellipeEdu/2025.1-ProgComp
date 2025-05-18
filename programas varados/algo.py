92ab6bd0afa2c6b6eca5091e5634a8716a9ac401
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-18 10:35:46 -0300

    teste print char

--- programas varados/algo.py
+++ programas varados/algo.py
@@ -2,7 +2,10 @@
 
 # print('testando o commit\n')
 
+'''
 numero = int(input('Digite seu numero:'))
 nome = (input('digite seu nome:'))
 
 print('seu nome e numero é: ', nome, '-', numero)
+'''
+print(chr(10084)) # emoji de coração (caractere unicode)
