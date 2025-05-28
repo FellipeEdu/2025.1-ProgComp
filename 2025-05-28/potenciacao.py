39014cba9e1c8010495b6532e38dd6e51dc9ffe5
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-28 17:12:06 -0300

    cod feito

---
+++ 2025-05-28/potenciacao.py
@@ -0,0 +1,25 @@
+import sys
+
+try:
+    base = int(input('digite a base: '))
+    potencia = int(input('digite a potencia: '))
+
+except ValueError:
+    sys.exit('ERRO: digite um valor numérico inteiro positivo.')
+
+except Exception as exc:
+    sys.exit(f'ERRO: {exc}.')
+
+else:
+    # condições de valor > 0 aqui
+    if potencia <= 0:
+        sys.exit('ERRO: digite uma base positiva.')
+
+    if potencia <= 0:
+        sys.exit('ERRO: digite uma potencia positiva.')
+
+    potenciacao = 0
+    contador = 1
+    while contador <= potencia:
+        potenciacao *= base
+        contador += 1
