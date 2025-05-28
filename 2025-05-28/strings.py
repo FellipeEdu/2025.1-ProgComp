a94c8f375c68e22b299c085e38735f80114b0f61
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-28 17:12:13 -0300

    cod feito

---
+++ 2025-05-28/strings.py
@@ -0,0 +1,7 @@
+strPalavraChave = '123Mudar'
+strPalavraInfo = ''
+
+while strPalavraInfo != strPalavraChave:
+    strPalavraInfo = input('Digite a Palavra Chave: ')
+
+print('Você digitou a Palavra Chave...')
