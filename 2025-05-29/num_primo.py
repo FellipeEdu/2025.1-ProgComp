045995fad071053760f0cc2969811fb7ad022e10
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-05-29 17:09:30 -0300

    while, contador, print

--- 2025-05-29/num_primo.py
+++ 2025-05-29/num_primo.py
@@ -10,3 +10,16 @@ except ValueError:
 
 except Exception as exc:
     sys.exit(f'ERRO: {exc}')
+
+else:
+    divisores = 1
+    contDivisor = 0
+    while divisores <= numero:
+        if (numero % divisores) == 0:
+            contDivisor += 1
+        divisores += 1
+
+    if contDivisor == 2:
+        print(f'{numero} é primo.')
+    else:
+        print(f'{numero} não é primo')
