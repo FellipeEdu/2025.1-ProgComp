596ccbad0ee1fea95a2ca970b2d68f2bf0810690
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-06-05 11:38:23 -0300

    codigo criado (exibe termos da pg, classif pg, soma termos pg, termo selecionado)

--- 2025-06-04 (exerc github)/04_progG.py
+++ 2025-06-04 (exerc github)/04_progG.py
@@ -1,8 +1,8 @@
 import sys
 
 try:
-	termoInicial = int(input('digite o 1° termo da PG: '))
-	razao = int(input('digite a razão da PG: '))
+	termoInicial = float(input('digite o 1° termo da PG: '))
+	razao = float(input('digite a razão da PG: '))
 	elementos = int(input('quantidade de elementos da PG: '))
 
 	if razao == 0:
@@ -15,4 +15,23 @@ except Exception as exc:
     sys.exit(f'ERRO: {exc}.')
 
 else:
-      for elementos in range():
+	for contador in range(1, elementos + 1, 1):
+		termoGeral = termoInicial * (razao ** (contador - 1))
+		print(f'{contador}° Elemento = {termoGeral}')
+
+	if razao > 1:
+		print('\nEssa PG é crescente.')
+	elif razao < 1 and razao > 0:
+		print('Essa PG é descrescente.')
+	elif razao == 1:
+		print('Essa PG é constante.')
+	elif razao < 0:
+		print('Essa PG é oscilante/alternada.')
+
+	soma = (termoInicial * ((razao ** elementos) - 1)) / (razao - 1)
+	print(f'Soma dos {elementos} primeiros elementos da PG = {soma}\n')
+
+	termoDesejado = int(input('informe um elemento desejado da PG: '))
+
+	termoGeralDesejado = termoInicial * (razao ** (termoDesejado - 1))
+	print(f'{termoDesejado}° Termo = {termoGeralDesejado}')
