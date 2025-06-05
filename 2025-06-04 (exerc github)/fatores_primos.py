0a1a233bb028cf174370f51e64348470e06e5058
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-06-05 10:03:50 -0300

    logica

--- 2025-06-04 (exerc github)/fatores_primos.py
+++ 2025-06-04 (exerc github)/fatores_primos.py
@@ -1,10 +1,10 @@
 import sys
 
 try:
-    fatores = int('digite quantos fatores primos voce quer: ')
+    num_fatores = int('digite quantos fatores primos voce quer: ')
 
-    if fatores <= 0:
-            sys.exit('ERRO: o numero deve ser maior que zero.')
+    if num_fatores < 2:
+            sys.exit('ERRO: o numero de fatores deve ser maior que 1.')
 
 except ValueError:
     sys.exit('ERRO: digite um valor numérico inteiro positivo.')
@@ -13,5 +13,15 @@ except Exception as exc:
     sys.exit(f'ERRO: {exc}.')
 
 else:
-     print(f'Os {fatores} numeros com {fatores} fatores primos são: ')
-     
+    print(f'Os {num_fatores} primeiros numeros com {num_fatores} fatores primos são: ')
+
+    for contadorNumeros in range(1, num_fatores + 1, 1): # vai ocontrolar o calc dos N primeiros numeros q podem ser decompostos em N fatores
+        #numero = 2
+        for numero in range(2, 1000, 1): # vai controlar o calculo de varios numeros verificando quais podem ser decompostos por N valores primos         
+            divisorPrimo = 2
+            while divisorPrimo <= numero: # verificar se é primo
+                if (numero % divisorPrimo) == 0:
+                    contDivisor += 1
+                    break
+                else:
+                    divisores += 1
