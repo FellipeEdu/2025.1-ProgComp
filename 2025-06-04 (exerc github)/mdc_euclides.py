13b954c711fb18da02d54e562e2f67812cda15bd
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-06-05 08:57:26 -0300

    add restriçoes

--- 2025-06-04 (exerc github)/mdc_euclides.py
+++ 2025-06-04 (exerc github)/mdc_euclides.py
@@ -4,23 +4,36 @@ try:
     numA = int(input('digite o 1° numero: '))
     numB = int(input('digite o 2° numero: '))
 
+    if numA < numB:
+        sys.exit('ERRO: o 1° numero deve ser maior que o 2°.')
+
 except ValueError:
     sys.exit('ERRO: digite um valor numérico inteiro positivo.')
 
 except Exception as exc:
     sys.exit(f'ERRO: {exc}.')
 
-else:     
+else:
     dividendo = numB
-    # quociente = numA // numB
-    resto_div = numA % numB # 270 % 210 = 60
+    resto_divisor = numA % numB # 60
 
-    for cont in range(0, 100, 1):
-        resto_final = dividendo % resto_div # 210 % 60 = 30
-        if resto_final == 0:
-            break
-        else:
-            dividendo = resto_div # dividendo = 60
-            resto_div = resto_final # resto div = 30
-        
-    print(f'MDC de {numA} e {numB} é: {resto_div}')
+    '''
+    while resto_final <= 0:
+        resto_final = dividendo % resto_divisor # 210 % 60 = 30 | 60 % 30 = 0
+        dividendo = resto_divisor # div = 60
+        resto_divisor = resto_final # resto_div = 30
+    ''' 
+
+    if resto_divisor == 0: 
+        print(f'MDC de {numA} e {numB} = {numB}.') 
+
+    else:
+        for contador in range(0, 100, 1):
+            resto_final = dividendo % resto_divisor
+            if resto_final == 0:
+                break
+            else:
+                dividendo = resto_divisor
+                resto_divisor = resto_final
+    
+        print(f'MDC de {numA} e {numB} = {resto_divisor}.') 
