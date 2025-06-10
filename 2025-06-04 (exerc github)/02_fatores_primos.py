94feb8b7260f5c73d75b2c60da4cd450e3c7ab97
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-06-10 17:15:15 -0300

    trabalhando

--- 2025-06-04 (exerc github)/02_fatores_primos.py
+++ 2025-06-04 (exerc github)/02_fatores_primos.py
@@ -1,7 +1,7 @@
 import sys
 
 try:
-    num_fatores = int('digite quantos fatores primos voce quer: ')
+    num_fatores = int('digite a quantidade de fatores primos que voce quer: ')
 
     if num_fatores < 2:
             sys.exit('ERRO: o numero de fatores deve ser maior que 1.')
@@ -13,6 +13,30 @@ except Exception as exc:
     sys.exit(f'ERRO: {exc}.')
 
 else:
+    intContFatores = 0
+    boolDivisoresConsec = False
+    intDivisao = 0
+    intDividendos = 2
+    #intDivisorPrimo = 2 # primeiro numero primo
+
+    while intContFatores <= num_fatores:
+        while boolDivisoresConsec == False:
+            # verificador de divisao primo
+            for intDivisorPrimo in range(2, intDividendos + 1, 1):
+                contVerifDivisorPrimo = 0
+
+                for intVerificadorDivisorPrimo in range(1, intDivisorPrimo + 1, 1):
+                    if (intDivisorPrimo % intVerificadorDivisorPrimo) == 0:
+                        contVerifDivisorPrimo += 1
+                    else:
+                        continue
+
+                if contVerifDivisorPrimo == 2: # agora vai dividir o intDividendos por um numero primo
+                    if (intDividendos % intDivisorPrimo) == 0:
+
+
+                         
+
     '''
     print(f'Os {num_fatores} primeiros numeros com {num_fatores} fatores primos são: ')
 
