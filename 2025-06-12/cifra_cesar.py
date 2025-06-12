bd9031bf00aee73a474fe13866876bb3f9968c6d
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-06-12 11:35:20 -0300

    verificando se ultrapassou tabela ascii

--- 2025-06-12/cifra_cesar.py
+++ 2025-06-12/cifra_cesar.py
@@ -6,5 +6,14 @@ strCifra = ''
 print(f'Cifra com deslocamento {intDeslocamento}: ', end='')
 
 for contador in range(0, len(strPalavra)):
-    strCifra = chr(ord(strPalavra[contador]) + intDeslocamento)
+
+    intCorrecao = 0
+    intString = ord(strPalavra[contador]) + intDeslocamento
+
+    if (intString) > 126:
+        intCorrecao = (intString - 127) + 33
+        strCifra = chr(intCorrecao)
+    else:
+        strCifra = chr(intString)
+
     print(strCifra, end='')
