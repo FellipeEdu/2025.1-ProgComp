0aa050b57ab9e50463c3b5d499beaabec66b6618
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-06-12 10:50:50 -0300

    print com end

--- 2025-06-12/cifra_cesar.py
+++ 2025-06-12/cifra_cesar.py
@@ -3,5 +3,8 @@ intDeslocamento = int(input('Deslocamento Desejado: '))
 
 strCifra = ''
 
-for contador in range(0, len(strPalavra) + 1):
-    strCifra += chr(ord(strPalavra[0]) + intDeslocamento)
+print(f'Cifra com deslocamento {intDeslocamento}: ', end='')
+
+for contador in range(0, len(strPalavra)):
+    strCifra = chr(ord(strPalavra[contador]) + intDeslocamento)
+    print(strCifra, end='')
