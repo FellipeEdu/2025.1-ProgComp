ee9c9947645392fd918f4affa3b738e090b0d01e
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-06-18 17:45:37 -0300

    arq criado

---
+++ 2025-06-18 (listas)/exemplo_04_print.py
@@ -0,0 +1,24 @@
+'''
+Fazer umporgrama que solicite ao usuario nomes de pessoas.
+
+O programa deverá parar de solicitar nomes quando o usuário digitar 'FIM'
+
+No final o programa deverá listar os nomes informados em ordem alfabetica.
+'''
+
+lstNomes = list()
+
+while True:
+    strNome = input('Digite um nome: ').strip()
+
+    if strNome.lower() == 'fim': 
+        break
+
+    if len(strNome) > 0:
+        lstNomes.append(strNome)
+
+#lstNomes.sort()
+lstNomes.sort()
+
+for strNome in lstNomes:
+    print(strNome)
