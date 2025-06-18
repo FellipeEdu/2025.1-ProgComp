666d16357fe8420b4bd7f20b55bb5a38f43bf301
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-06-18 17:28:26 -0300

    comentario e strNome = '' removido

--- 2025-06-18 (listas)/exemplo_01.py
+++ 2025-06-18 (listas)/exemplo_01.py
@@ -1,6 +1,12 @@
-lstNomes = list()
+'''
+Fazer umporgrama que solicite ao usuario nomes de pessoas.
 
-strNome = ''
+O programa deverá parar de solicitar nomes quando o usuário digitar 'FIM'
+
+No final o programa deverá listar os nomes informados.
+'''
+
+lstNomes = list()
 
 while True:
     strNome = input('Digite um nome: ').strip()
