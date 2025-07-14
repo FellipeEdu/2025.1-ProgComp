b2156b6a128a45fb9845e11c5580b64e87028360
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-07-14 12:00:27 -0300

    terminando cripto (falta descrpto)

--- Codigos Brutos da av unid ii/questao09.py
+++ Codigos Brutos da av unid ii/questao09.py
@@ -7,23 +7,49 @@ anim id est laborum."'''
 strCaracteres = '''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzáàâãäåçéèêíìîóòôõöúùûüÁÀÂÃÄÅÇÉÈÊÍÌÎÓÒÔÕÖÚÙÛÜ0123456789 !\"#$%&'()*+,-./:;<=>?@[\\]^_{|}~'''
 chaveAleatoria = '%_AC1D=RA1N%_8'
 strCriptografada = ''
+strCriptografadaChaveada = ''
+strCriptografadaNova = ''
+strCriptografadaChaveadaNova = ''
+indiceCripto = 0 
 
 print(f'Texto original:\n{strTexto}\n')
 
 # criptografar com a primeira chave
-indiceCripto = 0
 for letra in range(len(strTexto)):
     if indiceCripto == len(chaveAleatoria): indiceCripto = 0
     strCriptografada += chaveAleatoria[indiceCripto]
     indiceCripto += 1
+    if letra + 1 == len(strTexto): indiceCripto = 0
+
+print(f'Texto criptografado com primeira chave:\n{strCriptografada}\n')
 
 # criar alfabeto cifrado
-strCriptografadaChaveada = ''
-for letra in strTexto:
-    letraTemporaria = strCaracteres.find(letra)
+for letra in range(len(strTexto)): # usando range(), pois strTexto e strCriptografada tem o mesmo tam
+    letraTemporaria = strCaracteres.find(strTexto[letra])
     strAlfabeto = strCaracteres[letraTemporaria:] + strCaracteres[:letraTemporaria]
 
-    strTemporaria = strAlfabeto[strCaracteres.find(letra)]
+    strTemporaria = strAlfabeto[strCaracteres.find(strCriptografada[letra])]
     strCriptografadaChaveada += strTemporaria
 
-print(f'Texto criptografado:\n{strCriptografadaChaveada}\n')
+chaveNova = strCriptografadaChaveada[-9:]
+
+print(f'Texto criptografado 1:\n{strCriptografadaChaveada}\n')
+
+# criptografar com a segunda (nova) chave
+for letra in range(len(strCriptografadaChaveada)):
+    if indiceCripto == len(chaveNova): indiceCripto = 0
+    strCriptografadaNova += chaveNova[indiceCripto]
+    indiceCripto += 1
+    if letra + 1 == len(strCriptografadaChaveada): indiceCripto = 0
+
+print(f'Texto criptografado com nova chave:\n{strCriptografadaNova}\n')
+
+# usando a chave nova em strTexto
+for letra in range(len(strTexto)):
+    letraTemporaria = strCaracteres.find(strTexto[letra])
+    strAlfabeto = strCaracteres[letraTemporaria:] + strCaracteres[:letraTemporaria]
+
+    strTemporaria = strAlfabeto[strCaracteres.find(strCriptografadaNova[letra])]
+    strCriptografadaChaveadaNova += strTemporaria
+
+print(f'Criptografia com Nova chave:\n{strCriptografadaChaveadaNova}\n')
