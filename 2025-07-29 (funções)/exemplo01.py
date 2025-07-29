ffe6b735ef514b2df05ee6722bd31f7bf7638831
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-07-29 17:07:34 -0300

    desenvolvendo

--- 2025-07-29 (funções)/exemplo01.py
+++ 2025-07-29 (funções)/exemplo01.py
@@ -1,5 +1,13 @@
-def mediaIFRN(nota1, nota2):
-    media = (nota1 * 2 + nota2 * 3) / 5
-    return media
+from logging import exception
+import funcoes
 
-    
+try:
+    intE1 = int(input('Informe E1: '))
+    intE2 = int(input('Informe E2: '))
+except ValueError:
+    print('ERRO: Informe inteiro válido')
+except Exception as erro:
+    print(f'ERRO: {erro}')
+else:
+    mediaFinal = funcoes.mediaIFRN(intE1, intE2)
+    print(f'MÉDIA = {mediaFinal}')
---
+++ 2025-07-29 (funções)/funcoes.py
@@ -0,0 +1,13 @@
+def mediaIFRN(nota1, nota2):
+    if not isinstance(nota1, int):
+        raise ValueError('Informe Nota 1 inteiro')
+    if not isinstance(nota2, int):
+        raise ValueError('Informe Nota 2 inteiro')
+        
+    if nota1 < 0 or nota1 > 100:
+        raise Exception ('Nota 1 deve ser entre 0 e 100')
+    if nota2 < 0 or nota2 > 100:
+        raise Exception ('Nota 1 deve ser entre 0 e 100')
+
+    media = (nota1 * 2 + nota2 * 3) / 5
+    return media
