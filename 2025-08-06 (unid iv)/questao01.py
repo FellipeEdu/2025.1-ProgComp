648e6318d5a0f7756ffeb4b1f227fe85ff7fc974
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-08-06 17:57:01 -0300

    fazendo

--- 2025-08-06 (unid iv)/questao01.py
+++ 2025-08-06 (unid iv)/questao01.py
@@ -1,4 +1,4 @@
-import requests
+import requests; from datetime import datetime
 
 strURLMoedas  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata'
 strURLMoedas += '/Moedas?$top=100&$format=json'
@@ -12,3 +12,4 @@ strURLCotacoes += '@moeda=%27USD%27&@dataInicial=%2701-01-2023%27&'
 strURLCotacoes += '@dataFinalCotacao=%2712-31-2023%27&$format=json'
 
 dicCotacoes = requests.get(strURLCotacoes).json()
+print(dicCotacoes)
