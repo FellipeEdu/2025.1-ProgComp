2d2210c8e468762992b4bb7d8db568160c23bea9
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-08-06 17:29:41 -0300

    cod novos

---
+++ 2025-08-06 (unid iv)/questao01.py
@@ -0,0 +1,14 @@
+import requests
+
+strURLMoedas  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata'
+strURLMoedas += '/Moedas?$top=100&$format=json'
+
+dicMoedas = requests.get(strURLMoedas).json()
+
+strURLCotacoes  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/'
+strURLCotacoes += 'CotacaoMoedaPeriodo(moeda=@moeda,dataInicial='
+strURLCotacoes += '@dataInicial,dataFinalCotacao=@dataFinalCotacao)?'
+strURLCotacoes += '@moeda=%27USD%27&@dataInicial=%2701-01-2023%27&'
+strURLCotacoes += '@dataFinalCotacao=%2712-31-2023%27&$format=json'
+
+dicCotacoes = requests.get(strURLCotacoes).json()
