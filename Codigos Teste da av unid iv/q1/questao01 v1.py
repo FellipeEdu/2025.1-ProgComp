59f4f438b48931665f3bfdabe4942183dcc02efb
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-08-11 14:37:17 -0300

    mudanças q1

--- Codigos Teste da av unid iv/q1/questao01 v1.py
+++ Codigos Teste da av unid iv/q1/questao01 v1.py
@@ -1,15 +1,44 @@
-import requests; from datetime import datetime
+import sys, requests; 
+from datetime import datetime
+from funcoes01 import *
 
 strURLMoedas  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata'
 strURLMoedas += '/Moedas?$top=100&$format=json'
 
-dicMoedas = requests.get(strURLMoedas).json()
+try:    
+    dicMoedas = requests.get(strURLMoedas).json()
+except requests.RequestException as erro:
+    sys.exit(f"ERR0: Falha na requisição à API: {erro}")
 
-strURLCotacoes  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/'
-strURLCotacoes += 'CotacaoMoedaPeriodo(moeda=@moeda,dataInicial='
-strURLCotacoes += '@dataInicial,dataFinalCotacao=@dataFinalCotacao)?'
-strURLCotacoes += '@moeda=%27USD%27&@dataInicial=%2701-01-2023%27&'
-strURLCotacoes += '@dataFinalCotacao=%2712-31-2023%27&$format=json'
+# listando moedas válidas
+lstMoedas = [moeda['simbolo'] for moeda in dicMoedas['value']]
 
-dicCotacoes = requests.get(strURLCotacoes).json()
-print(dicCotacoes)
+print('----- CONSULTA DE COTAÇÕES -----')
+outraConsulta = None
+while not outraConsulta == 'N':
+    try:
+        anoEscolhido = int(input('Digite o ano para consulta: '))
+        while anoEscolhido > datetime.today().year or anoEscolhido < 1994:
+            anoEscolhido = int(input('ERRO: Digite um ano válido\nDigite o ano: '))
+
+        moedaEscolhida = input('Escolha uma das moedas abaixo (ex.: CAD)\n[AUD, CAD, CHF, DKK, EUR, GBP, JPY, NOK, SEK, USD]: ').upper()
+        while not moedaEscolhida in lstMoedas:
+            moedaEscolhida = input('ERRO: Escolha uma moeda válida.\nEscolha uma moeda: ').upper()
+
+        reqHTTP = moedaAno(anoEscolhido, moedaEscolhida)
+
+        try:
+            dicCotacoes = requests.get(reqHTTP).json()
+        except requests.RequestException as erro:
+            sys.exit(f"ERR0: Falha na requisição à API: {erro}")   
+    except Exception as erro:
+        sys.exit(f'\nERRO: {erro}')
+    else:
+        mediaMeses = calcMediasMensais(dicCotacoes)
+        
+        cotacaoCSV = escreverJSONeCSV( anoEscolhido, moedaEscolhida, mediaMeses)
+        print(cotacaoCSV)
+
+    outraConsulta = input('\nDeseja fazer outra consulta?\nSim [S] ou Não [N]: ').upper()
+    while not (outraConsulta == 'S' or outraConsulta == 'N'):
+        outraConsulta = input('\nERRO: digite um valor válido.\nDeseja visualizar de novo? ').upper()
