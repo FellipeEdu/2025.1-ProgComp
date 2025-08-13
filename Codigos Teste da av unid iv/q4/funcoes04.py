7136e8504fe4754921b55fd55a0ca8a803a08a01
Fellipe Eduardo <fellipeeduardo4@gmail.com>
2025-08-12 23:10:44 -0300

    QUASEEE

--- Codigos Teste da av unid iv/q4/funcoes04.py
+++ Codigos Teste da av unid iv/q4/funcoes04.py
@@ -8,7 +8,7 @@ API Key abaixo, obtida criando uma conta no site da API: https://home.openweathe
 '''
 API_KEY = '76b7682ab999cc218154b6340a6f1451'
 
-def obterPrevisao(cidade:str, numDias = 1): # 1 por padrão (clima do 'dia atual')
+def obterPrevisao(cidade:str, numDias = 1) -> list: # 1 por padrão (clima do 'dia atual')
     '''
     Obtém previsão do tempo a partir dos parâmetros 'cidade' e 'numDias' (número de dias) para previsões dos próximos dias. 
     É limitado a 5 dias devido função gratuita da API. Retorna uma lista com um único elemento do tipo dicionário contendo a previsão
@@ -18,32 +18,39 @@ def obterPrevisao(cidade:str, numDias = 1): # 1 por padrão (clima do 'dia atual
 
     try:
         print(f"{'-'*40}\nRequisitando dados da API...")
-        try:
-            dadosPrevisao = requests.get(urlPrevisao).json()
-        except requests.RequestException as erro:
-            sys.exit(f"ERRO: Falha na requisição à API: {erro}")
-        except json.JSONDecodeError as erro:
-            print(f"ERRO ao ler o arquivo JSON: {erro}. Provavelmente a cidade não existe, ou o nome inserido não é uma cidade.")
-            return []
-
+        dadosPrevisao = requests.get(urlPrevisao).json()
+        lstPrevisoesIniciais = []
         lstPrevisoesSelec = []
-        for i in range(numDias + 1): # Pega os próximos dias (ex: 1 + 1 = range 2, vai pegar a previsão até i = 1, que é amanhã)            
-            previsaoDia = dadosPrevisao['list'][i*8] # Pega a primeira previsão do dia
+        dicPrevisoes = {}
+
+        for previsao in dadosPrevisao['list']:
+            testeData = datetime.strptime(previsao['dt_txt'], '%Y-%m-%d %H:%M:%S')
+            if testeData.hour == 12:
+                lstPrevisoesIniciais.append(previsao)
+
+        for dicPrevisao in lstPrevisoesIniciais[:numDias]: # Percorre cada dia
+            try:               
+                previsaoDia = dicPrevisao # [i*8] # Pega a primeira previsão do dia
+            except requests.RequestException as erro:
+                print(f"ERRO: Falha na requisição à API: {erro}")
+                return []
+            except KeyError as erro:
+                print(f"ERRO: {erro}. Nome da cidade incorreto ou cidade inexistente.")
+                return []
             dataPrevisao = previsaoDia['dt_txt']
             tempDia = previsaoDia['main']['temp']
             descricaoDia = previsaoDia['weather'][0]['description']
             umidadeAtual = previsaoDia['main']['humidity']
+            icone = previsaoDia['weather'][0]['icon']
 
             dicPrevisoes = {
                 'data_Previsao': dataPrevisao,
                 'temperatura': tempDia,
                 'descricao': descricaoDia,
-                'umidade': umidadeAtual
+                'umidade': umidadeAtual,
+                'icone': f'https://openweathermap.org/img/wn/{icone}@2x.png'
             }   
-            '''print(f"Data: {datetime.fromtimestamp(dataPrevisao).strftime('%d/%m')}")
-            print(f"  Descrição: {descricaoDia.capitalize()}")
-            print(f"  Temperatura: {tempDia}°C")
-            print(f"Umidade: {umidadeAtual}%")'''
+
             lstPrevisoesSelec.append(dicPrevisoes)
 
         return lstPrevisoesSelec
@@ -51,16 +58,14 @@ def obterPrevisao(cidade:str, numDias = 1): # 1 por padrão (clima do 'dia atual
     except requests.exceptions.RequestException as erro:
         print(f"Erro na requisição de Previsão: {erro}")
 
-
-#def dadosPrevisoes(previsaoAtual, previsaoDias):
 def dadosPrevisoes(cidade:str, previsaoDiarias):
     '''
-    Cria um dicionário completo para uma consulta de histórico,
-    usando apenas os dados da previsão.
+    Cria um dicionário completo para adicionar ao histórico,
+    usando apenas 'previsaoDiarias' (dados das previsões retornadas) e 'cidade'.
     '''
     if cidade and previsaoDiarias:
         nova_consulta = {
-            'data_hora': datetime.now().strftime('%d/%m/%Y %H:%M:%S'), # datetime.fromtimestamp(previsaoDiarias[0]['data_Previsao']).strftime('%d/%m/%Y %H:%M:%S')
+            'data_hora': datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
             'localizacao': cidade,
             'previsoes_selecionadas': previsaoDiarias # Salva a lista de previsões
         }
@@ -82,7 +87,7 @@ def salvarHistorico(historico, nomeArq="historico.json"):
 
 def lerHistorico(nomeArq="historico.json"):
     '''
-    Lê o histórico de consultas de um arquivo JSON.
+    Lê o histórico de consultas de um arquivo JSON para que, posteriormente no programa, adicione um novo histórico.
     Retorna uma lista vazia se o arquivo não existir.
     '''
     try:
--- Codigos Teste da av unid iv/q4/questao04.py
+++ Codigos Teste da av unid iv/q4/questao04.py
@@ -1,3 +1,16 @@
+'''
+Pesquisa e documentação:
+Informe o nome da API escolhida: https://openweathermap.org/
+Explique o que a API fornece (quais dados ou serviços): A API retorna previsão do tempo para até 5 dias.
+Indique a documentação consultada (link oficial ou repositório): https://openweathermap.org/forecast5
+
+Descreva o objetivo do seu programa: o que ele fará utilizando essa API:
+- O usuário poderá verificar a previsão do tempo para qualquer cidade no mundo (digitando o nome em português) (por ex: Natal), especificando com um 
+código de país se preferir (ex: Londres GB; veja uma lista de códigos de países no link a seguir, selecionando a opção 'Country Codes'
+no menu lateral à esquerda: https://www.iso.org/obp/ui/#search).
+- Salvar e Carregar histórico de cada consulta realizada.
+'''
+
 import sys, locale; 
 from datetime import datetime
 from funcoes04 import *
@@ -8,12 +21,12 @@ print(f"{'-' * 20} PREVISÃO DO TEMPO {'-' * 20}")
 outraConsulta = None
 while not outraConsulta == 'N':
     historicoConsultas = lerHistorico()
-    resultado = None
+    resultadoPrevisao = None
 
     while True:
         try:
             cidade = input('\nDigite o nome de uma cidade (ex.: Macaiba): ')
-            numDias = int(input('\nConsultar Previsão de Hoje (1) a até (5) Dias\nEscolha de 1 a 4 dias: '))
+            numDias = int(input('\nConsultar Previsão de Hoje (1) a até (5) Dias\nEscolha de 1 a 5 dias: '))
             if 1 <= numDias <= 5: break
             else: print(f"ERRO: informe opção válida.")
         except ValueError:
@@ -21,16 +34,16 @@ while not outraConsulta == 'N':
         except Exception as erro:
             sys.exit(f'\nERRO: {erro}')
 
-    resultado = obterPrevisao(cidade, numDias)
-    print(f"\nPrevisão para Hoje e para o(s) Próximo(s) {numDias} Dia(s):\n{'-'*40}")
-    for previsao in resultado:
-        print(f"Data: {datetime.strptime(previsao['data_Previsao'], '%Y-%m-%d %H:%M:%S').strftime('%A, %d/%m, %Hh')}") # {datetime.fromtimestamp(previsao['data_Previsao']).strftime('%A, %d/%m')}
+    resultadoPrevisao = obterPrevisao(cidade, numDias)
+    print(f"\nPrevisão para Hoje e para o(s) Próximo(s) {numDias} Dia(s):\n{'-'*60}")
+    for previsao in resultadoPrevisao:
+        print(f"Data: {datetime.strptime(previsao['data_Previsao'], '%Y-%m-%d %H:%M:%S').strftime('%A, %d/%m')}")
         print(f"Temperatura: {previsao['temperatura']:.1f}°C")
-        print(f"Descrição: {previsao['descricao'].capitalize()}")
-        print(f"Umidade: {previsao['umidade']}%\n{'-'*40}")
+        print(f"Descrição: {previsao['descricao'].capitalize()} | {previsao['icone']}")
+        print(f"Umidade: {previsao['umidade']}%\n{'-'*60}")
 
     # salvando no histórico
-    historicoConsultas.append(dadosPrevisoes(cidade, resultado))
+    historicoConsultas.append(dadosPrevisoes(cidade, resultadoPrevisao))  
     salvarHistorico(historicoConsultas)
 
     outraConsulta = input('\nDeseja fazer outra consulta?\nSim [S] ou Não [N]: ').upper()
